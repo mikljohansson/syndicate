87d33cb0d14c887cd11f95c380460b53237b4262
Mikael Johansson <mikl.johansson@gmail.com>
2018-08-31 11:54:57 +0200

    Refactored GDPR cleanup into a task that has retry and backoff logic in case of errors

--- core/script/gdpr_issue_cleanup.php
+++ core/script/gdpr_issue_cleanup.php
@@ -1,32 +1,7 @@
 <?php
-set_include_path(get_include_path().':'.dirname(dirname(dirname(__FILE__))).':'.dirname(dirname(dirname(dirname(__FILE__)))).'/synd');
+set_include_path(get_include_path().':'.dirname(dirname(dirname(dirname(__FILE__)))).'/local'.':'.dirname(dirname(dirname(__FILE__))));
 require_once 'synd.inc';
 
-// List of issues keyword names to clean up
-$keywords = array('GDPR-gallring');
-
-$sql = "
-	SELECT i.node_id 
-	FROM synd_issue i, synd_project p
-	WHERE 
-		i.project_node_id = p.node_id AND
-		i.ts_update <= (".time()." - p.info_cleanup_cutoff_seconds)
-
-	UNION ALL
-	
-	SELECT i.node_id 
-	FROM synd_issue i, synd_issue_keyword ik, synd_keyword k 
-	WHERE
-		i.node_id = ik.issue_node_id AND
-		k.node_id = ik.keyword_node_id AND
-		k.info_head IN (".implode(',', $synd_maindb->quote($keywords)).")";
-
-//$ids = $synd_maindb->getCol($sql, 0, 0, 1);
-$ids = array('issue.1934540');
-$issues = SyndNodeLib::getInstances($ids);
 $module = Module::getInstance('issue');
-
-foreach ($issues as $issue) {
-	$module->cleanup($issue);
-}
-
+$module->runCleanup();
+//system('killall php');
